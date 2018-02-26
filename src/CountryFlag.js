bc6e53676be0d7c7e56e12a7bcbb6b3193943e02
Dmitry Vinnik <dmitryvinn@users.noreply.github.com>
2018-02-26 00:42:40 -0800

    Making app responsive

--- src/CountryFlag.js
+++ src/CountryFlag.js
@@ -1,4 +1,5 @@
 import React from "react";
+var MediaQuery = require("react-responsive").default;
 
 const imageStyle = {
   height: "300px",
@@ -8,8 +9,34 @@ const imageStyle = {
 
 class CountryFlag extends React.Component {
   render() {
-    return <img style={imageStyle} src={this.props.img} alt="Country Flag" />;
+    return (
+      <div>
+        <MediaQuery query="(min-device-width: 1224px)">
+          <img style={imageStyle} src={this.props.img} alt="Country Flag" />
+        </MediaQuery>
+        <MediaQuery query="(min-device-width: 1223px)">
+          <img
+            style={{ visibility: "hidden" }}
+            src={this.props.img}
+            alt="Country Flag"
+          />
+        </MediaQuery>
+        <MediaQuery query="(max-device-width: 1222px)">
+          <img
+            style={{ display: "none" }}
+            src={this.props.img}
+            alt="Country Flag"
+          />
+        </MediaQuery>
+      </div>
+    );
   }
+
+  // render() {
+  //   return(
+
+  //   );
+  // }
 }
 
 export default CountryFlag;
